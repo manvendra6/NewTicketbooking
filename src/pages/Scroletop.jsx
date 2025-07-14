e46253439718a962cf8548dd1c4f6c912599debe
manvendra6 <patelmanvendra609@gmail.com>
2025-07-14 11:28:33 +0530

    first commit

--- src/pages/Scroletop.jsx
+++ src/pages/Scroletop.jsx
@@ -4,9 +4,11 @@ import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
+  console.log( "pathname",pathname)
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth'  } );
+  
   }, [pathname]);
 
   return null;
